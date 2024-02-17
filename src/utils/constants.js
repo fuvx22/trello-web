5cda46f36918826269684d14869a41adf297de13
fuvx22 <voxngocphu@gmail.com>
2024-02-17 21:36:28 +0700

    deploy vercel

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,14 @@
 // export const API_ROOT = 'http://localhost:8017'
-export const API_ROOT = 'https://trello-api-bi8b.onrender.com'
+
+let apiRoot = ''
+
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-bi8b.onrender.com'
+}
+
+console.log('API Root', apiRoot)
+
+export const API_ROOT = apiRoot
